b4680d773de411cb8547403dc8bd1ee3329820cd
srinivas <srinivasaraochinta@yahoo.com>
2020-09-18 00:01:11 +0530

    initial add

---
+++ src/main/java/com/wellsfargo/productgateway/ProductgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.productgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductgatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wellsfargo/productgateway/ProductgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.productgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
